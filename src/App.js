e54f3b243094d56ab172fcb4c5d842e3d7c2055b
prince-sunsara <princesunasara977@gmail.com>
2024-01-03 15:07:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, ButtonGroup } from "@mui/material";
+import { PhotoCamera } from "@mui/icons-material";
+
+import { makeStyles } from '@mui/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  }
+})
+
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      {/* <h1>Hello, World!</h1> */}
+      {/* <Typography variant="h1" >Hello World!</Typography>
+      <Typography variant="h3" >Hello World!</Typography> */}
+
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Hello everyone This is a photo album. you can check out what I work here to make it this material ui website
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">See My Photos</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">Secondary action</Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </ThemeProvider>
   );
 }
 
