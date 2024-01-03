f1489699aa76edc740a2c9e902efbb313c216e29
prince-sunsara <princesunasara977@gmail.com>
2024-01-03 16:30:11 +0530

    Material UI work done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, ButtonGroup } from "@mui/material";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
 import { ThemeProvider } from '@mui/material/styles';
 import { theme, useStyles } from "./styles";
+// import img from './img.jpeg'
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 function App() {
   const classes = useStyles();
@@ -16,7 +18,7 @@ function App() {
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
@@ -29,7 +31,7 @@ function App() {
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Hello everyone This is a photo album. you can check out what I work here to make it this material ui website
             </Typography>
-            <div>
+            <div className={classes.button}>
               <Grid container spacing={2} justifyContent="center">
                 <Grid item>
                   <Button variant="contained" color="primary">See My Photos</Button>
@@ -41,7 +43,43 @@ function App() {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    // image={img}
+                    image={`https://source.unsplash.com/random?count=${card}`}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">View</Button>
+                    <Button size="small" color="primary">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Something here is give to footer a purpose!
+        </Typography>
+      </footer>
     </ThemeProvider>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -6,6 +6,31 @@ const useStyles = makeStyles({
     container: {
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(8, 0, 6),
+    },
+    icon: {
+        marginRight: '20px',
+    },
+    button: {
+        marginTop: '40px'
+    },
+    cardGrid: {
+        padding: "20px 0"
+    },
+    card: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    cardMedia: {
+        paddingTop: "56.25%",  // 16:9
+        // paddingTop: "100%",
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: '50px 0'
     }
 })
 
